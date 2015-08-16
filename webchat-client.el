34f8e0fe1dcee3f586577b6fc5c6adb02301d9b2
lujun9972 <lujun9972@gmail.com>
2015-08-16 21:04:31 +0800

    拆分server和code代码

--- webchat-client.el
+++ webchat-client.el
@@ -1,48 +1,3 @@
-;; 以下是server端代码
-(require 'elnode)
-(defvar webchat-server--total-lines 0
-  "聊天室中总用有多少行内容")
-(defvar webchat-server-max-hold-lines 1000
-  "服务端最多保存多少行聊天内容")
-(defvar webchat-server--content-ring (make-ring webchat-server-max-hold-lines)
-  "聊天内容")
-(defun webchat-server--get-content-start-from (start)
-  "根据请求的起始行,产生实际应该返回的内容"
-  (let* ((new-content-lines (- webchat-server--total-lines start))
-		 (new-content-list (cond ((= new-content-lines 0)
-								  nil)
-								 (t (subseq (ring-elements webchat-server--content-ring) (- new-content-lines))))))
-	(apply #'concat new-content-list)))
-
-(defun webchat-server--get-content-handler (httpcon)
-  (let* ((start (string-to-number (or (elnode-http-param httpcon "start") "0")))
-		 (new-content (webchat-server--get-content-start-from start)))
-	(elnode-http-start httpcon 200 '("Content-Type" . "text/plain"))
-	(elnode-http-return httpcon (prin1-to-string (cons webchat-server--total-lines new-content)))))
-
-(defun webchat-server--say-handler (httpcon)
-  (let ((who (elnode-http-param httpcon "who"))
-		(content (elnode-http-param httpcon "content")))
-	(when (stringp content)
-	  (ring-insert-at-beginning webchat-server--content-ring (format "%s:\n\t%s\n" who content))
-	  (incf webchat-server--total-lines)))
-  (elnode-http-start httpcon 200 '("Content-Type" . "text/plain"))
-  ;; (elnode-http-start httpcon 302 '("Location" . "/"))
-  (elnode-http-return httpcon))
-
-(defconst webchat-urls
-  `(("^/$" . webchat-server--get-content-handler)
-	("^/update/.*$" . webchat-server--say-handler)))
-
-
-(defun webchat-server--dispatcher-handler (httpcon)
-  (elnode-dispatcher httpcon webchat-urls))
-
-(defun webchat-server()
-  (interactive)
-  (let ((port (read-number "请输入监听端口: ")))
-	(elnode-start 'webchat-server--dispatcher-handler :port port)))
-
 ;; 以下是client端代码
 (require 'url)
 (defvar webchat-client-server-host "localhost"
@@ -59,7 +14,7 @@
 	(with-current-buffer buf
 	  (goto-char (point-min))
 	  (search-forward-regexp "^$")
-	  (setq content (read-from-whole-string (buffer-substring-no-properties (+ (point )1) (point-max)))))
+	  (setq content (read-from-whole-string (buffer-substring-no-properties (+ (point ) 1) (point-max)))))
 	(kill-buffer buf)
 	(setq webchat-client--total-lines (car content))
 	(decode-coding-string (cdr content) 'utf-8-dos)))
@@ -130,4 +85,4 @@
   (select-window (get-buffer-window webchat-client-content-buffer))
   (kill-buffer-and-window))
 
-(provide 'webChat)
+(provide 'webchat-client)
---
+++ webchat-server.el
@@ -0,0 +1,46 @@
+;; 以下是server端代码
+(require 'elnode)
+(defvar webchat-server--total-lines 0
+  "聊天室中总用有多少行内容")
+(defvar webchat-server-max-hold-lines 1000
+  "服务端最多保存多少行聊天内容")
+(defvar webchat-server--content-ring (make-ring webchat-server-max-hold-lines)
+  "聊天内容")
+(defun webchat-server--get-content-start-from (start)
+  "根据请求的起始行,产生实际应该返回的内容"
+  (let* ((new-content-lines (- webchat-server--total-lines start))
+		 (new-content-list (cond ((= new-content-lines 0)
+								  nil)
+								 (t (subseq (ring-elements webchat-server--content-ring) (- new-content-lines))))))
+	(apply #'concat new-content-list)))
+
+(defun webchat-server--get-content-handler (httpcon)
+  (let* ((start (string-to-number (or (elnode-http-param httpcon "start") "0")))
+		 (new-content (webchat-server--get-content-start-from start)))
+	(elnode-http-start httpcon 200 '("Content-Type" . "text/plain"))
+	(elnode-http-return httpcon (prin1-to-string (cons webchat-server--total-lines new-content)))))
+
+(defun webchat-server--say-handler (httpcon)
+  (let ((who (elnode-http-param httpcon "who"))
+		(content (elnode-http-param httpcon "content")))
+	(when (stringp content)
+	  (ring-insert-at-beginning webchat-server--content-ring (format "%s:\n\t%s\n" who content))
+	  (incf webchat-server--total-lines)))
+  (elnode-http-start httpcon 200 '("Content-Type" . "text/plain"))
+  ;; (elnode-http-start httpcon 302 '("Location" . "/"))
+  (elnode-http-return httpcon))
+
+(defconst webchat-urls
+  `(("^/$" . webchat-server--get-content-handler)
+	("^/update/.*$" . webchat-server--say-handler)))
+
+
+(defun webchat-server--dispatcher-handler (httpcon)
+  (elnode-dispatcher httpcon webchat-urls))
+
+(defun webchat-server()
+  (interactive)
+  (let ((port (read-number "请输入监听端口: ")))
+	(elnode-start 'webchat-server--dispatcher-handler :port port)))
+
+(provide 'webchat-server)
