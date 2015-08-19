cf9dbe72db7335f9488f308c6129b7b36687ab28
darksun <lujun9972@sina.com>
2015-08-19 11:50:34 +0800

    以更函数式的形式重构代码

--- webchat-client.el
+++ webchat-client.el
@@ -1,15 +1,10 @@
+;;-*- lexical-binding:t -*-
 ;; 以下是client端代码
 (require 'url)
 (require 'thingatpt)
-(defvar webchat-client-server-host "localhost"
-  "webchat的服务器地址")
-(defvar webchat-client-server-port 8000
-  "webchat的服务器监听端口")
 (defvar webchat-client--total-lines 0
   "webchat客户端已经收到多少行聊天记录")
-(defun webchat-client--get-content(&optional host port)
-  (setq host (or host webchat-client-server-host))
-  (setq port (or port webchat-client-server-port))
+(defun webchat-client--get-content(host port)
   (let ((buf (url-retrieve-synchronously (format "http://%s:%s/?start=%s" host port webchat-client--total-lines) t))
 		content)
 	(unwind-protect 
@@ -21,9 +16,7 @@
 	(setq webchat-client--total-lines (car content))
 	(decode-coding-string (cdr content) 'utf-8-dos)))
 
-(defun webchat-client--say(who content &optional host port)
-  (setq host (or host webchat-client-server-host))
-  (setq port (or port webchat-client-server-port))
+(defun webchat-client--say(host port who content)
   (let ((url (format "http://%s:%s/update/"
 					 host
 					 port))
@@ -38,9 +31,9 @@
 				  t)))
 (defvar webchat-client-content-buffer "*webchat-content*"
   "显示聊天内容的buffer")
-(defun webchat-client--display-content()
+(defun webchat-client--display-content(host port)
   "在buffer内显示聊天内容"
-  (let ((content (webchat-client--get-content))
+  (let ((content (webchat-client--get-content host port))
 		(webchat-client-content-window (get-buffer-window (get-buffer-create webchat-client-content-buffer))))
 	(save-selected-window
 	  (save-excursion 
@@ -54,32 +47,33 @@
 (define-derived-mode webchat-mode text-mode "WebChat"
   "Major mode for running webchat"
   (make-local-variable 'scroll-step)
-  (setq scroll-step 2)
-  (local-set-key (kbd "<RET>") #'webchat-client--talk))
+  (setq scroll-step 2))
 
-(defvar webchat-client-who user-login-name
-  "webchat客户名")
-(defun webchat-client--talk ()
+(defun webchat-client--talk (host port who)
   (interactive)
   "Function called when return is pressed in interactive mode to talk"
   (let ((content (buffer-substring-no-properties (point-min) (point-max))))
-    (webchat-client--say webchat-client-who content)
+    (webchat-client--say host port who content)
 	(erase-buffer)))
 
 (defvar webchat-client--timer nil)
 (defvar webchat-client-talk-buffer "*webchat-talk*"
   "输入聊天内容的buffer")
-(defun webchat-talk ()
-  (interactive)
-  (setq webchat-client-server-host (read-string "请输入服务器地址: " webchat-client-server-host))
-  (setq webchat-client-server-port (read-number "请输入服务端口: " webchat-client-server-port))
-  (setq webchat-client-who (read-string "请输入你的名称: " webchat-client-who))
+(defun webchat-talk (host port who)
+  (interactive (list (read-string "请输入服务器地址: " "localhost")
+					 (read-number "请输入服务端口: " 8000)
+					 (read-string "请输入你的名称: " user-login-name)))
   (switch-to-buffer (get-buffer-create webchat-client-content-buffer))
   (select-window (split-window-below -4))
   (switch-to-buffer (get-buffer-create webchat-client-talk-buffer))
   (webchat-mode)
-  ;; (setq webchat-client--timer (run-with-idle-timer 1 0.3 #'webchat-client--display-content))
-  (setq webchat-client--timer (run-with-timer 1 0.3 #'webchat-client--display-content)))
+  (local-set-key (kbd "<RET>") (lambda ()
+								 (interactive)
+								 (webchat-client--talk host port who)))
+  ;; (setq webchat-client--timer (run-with-idle-timer 1 0.3 (lambda ()
+  ;; 														   (webchat-client--display-content host port))))
+  (setq webchat-client--timer (run-with-timer 1 0.3 (lambda ()
+													  (webchat-client--display-content host port)))))
 
 (defun webchat-quit ()
   (interactive)
