1511b9f689bfc83fbf6d085b488f57f0e01458f8
unknown <lujw@drcbank.com>
2015-09-01 11:54:14 +0800

    鎻愪緵澶歝hannel,姣忎釜channel瀵瑰簲涓€涓獁ebchat-server瀛愯繘绋嬫彁渚涙湇鍔?

--- webchat-client-push.el
+++ webchat-client-push.el
@@ -76,4 +76,34 @@
   (select-window (get-buffer-window webchat-client-content-buffer))
   (kill-buffer-and-window))
 
+(defun webchat-client--select-from-alist (alist &optional prompt)
+  ""
+  (unless prompt
+	(setq prompt ""))
+  (let* ((keys (mapcar #'car alist))
+		 ;; (ido-separator "\n")
+		 (key (ido-completing-read prompt keys))
+		 (value (cdr (assoc key alist))))
+	(cond ((atom value)
+		   value)
+		  (t (cdr value)))))
+
+
+(defun webchat-client(host port listener who)
+  (interactive (list (read-string "请输入服务器地址: " "localhost")
+					 (read-number "请输入服务端口: " 8000)
+					 (read-number "请输入客户端的监听端口: " 9000)
+					 (read-string "请输入你的名称: " user-login-name)))
+  (let ((p1 (make-network-process :name "webchat-dispatcher"
+								  :buffer "*webchat-dispatcher*"
+								  :family 'ipv4
+								  :host host
+								  :service port)))
+	(while (not (eq (process-status p1) 'closed))
+	  (accept-process-output p1 0.1))
+	(with-current-buffer (process-buffer p1)
+	  (setq port (webchat-client--select-from-alist  (car (read-from-string (buffer-substring-no-properties (point-min) (point-max)))) "channel: "))
+	  (delete-process p1)
+	  (kill-buffer))
+	(webchat-talk host port listener who)))
 (provide 'webchat-client)
---
+++ webchat-server-dispatcher.el
@@ -0,0 +1,27 @@
+(defvar webchat-server-topic-port-alist '(("cartoon" . 8001) ("emacs" . 8002))
+  "主题与端口的对应表")
+(defvar webchat-server-dispatcher-process nil)
+
+(defun webchat-server--build-server-process (port)
+  "创建webchat-server进程"
+  (let ((emacs-bin-path (concat invocation-directory invocation-name)))
+	(start-process "webchat-server" "webchat-server" emacs-bin-path "--script" "webchat-server.el" (number-to-string port))))
+
+
+(defun webchat-server-dispatcher (port)
+  (interactive `(,(read-number "请输入服务端口: " 8000)))
+  (when webchat-server-dispatcher-process
+	(delete-process webchat-server-dispatcher-process))
+  (setq webchat-server-dispatcher-process
+		(make-network-process :name "webchat-server-dispatcher"
+							  :family 'ipv4
+							  :server t
+							  :service port
+							  :log #'webchat-server-dispatch-topics))
+  (mapcar #'webchat-server--build-server-process (mapcar #'cdr webchat-server-topic-port-alist)))
+
+(defun webchat-server-dispatch-topics (server connection msg)
+  (process-send-string connection (prin1-to-string webchat-server-topic-port-alist))
+  (delete-process connection))
+
+;; (webchat-server-dispatcher 8000)
