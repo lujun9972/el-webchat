e24da81346bb79cc82709019203b39d03c5af492
lujun9972 <lujun9972@gmail.com>
2015-09-06 07:13:22 +0800

    高亮代码

--- webchat-mode.el
+++ webchat-mode.el
@@ -1,8 +1,16 @@
+(unless (and (boundp 'package--initialized)
+			 package--initialized)
+  (package-initialize))
 (define-derived-mode webchat-mode org-mode "WebChat"
   "Major mode for running webchat"
   (make-local-variable 'scroll-step)
   (setq scroll-step 2)
-  (org-indent-mode 1))
+  (org-indent-mode 1)
+  (if (require 'htmlize nil t)
+	  (progn 
+		(make-local-variable 'org-src-fontify-natively)
+		(setq org-src-fontify-natively t))
+	(warn "无法加载htmlize,无法使用本地代码高亮")))
 
 (defun webchat-build-window (content-buffer talk-buffer)
   "构建webchat窗口模式"
